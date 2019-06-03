5dc306b91b3f88e228b84565ad8884e9971b43f6
erandir <aefs12junior@gmail.com>
2019-06-03 12:50:59 -0300

    Updated example

--- example/index.php
+++ example/index.php
@@ -4,6 +4,6 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 
 $application = new \Symfony\Component\Console\Application();
 
-$application->add(new \EntityGenerator\EntityGenerator());
+$application->add(new \EntityGenerator\EntityGenerator('./'));
 
 $application->run();
